499748d32f9ea54f1a160a0d3db9d63cc1ca859f
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2023-11-26 15:53:56 +0200

    Created color and board files and classes, work in progress

---
+++ board.rb
@@ -0,0 +1,11 @@
+# U+2981,U+26AA,U+26AC,U+26AB, U+26AB possible red/white dots
+
+#U+220E, U+23FA, U+2460-5, U+25CF, U+278A-F possible peg
+
+
+class Board
+    def initialize
+        
+        @board = "\U+130C9"
+    end
+end
