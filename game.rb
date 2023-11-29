c06d1620b9c614fb0581b1be38afb5d262befbf7
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2023-11-29 21:24:12 +0200

    Added some more thoughts and stuff todo in comments

--- game.rb
+++ game.rb
@@ -20,3 +20,6 @@ end
 # Should get all the variables in Game class (controller). The board class
 # should consist of a method or two then, probably class methods, that take
 # arguments (some Game variables?) and then print board or w/e
+# Maybe write in main.rb first what is my game flow (.i.e get code, 
+# get user_guess, compare to code, check for round/number of guesses etc)
+# and the proceed to write the method/classes as needed
