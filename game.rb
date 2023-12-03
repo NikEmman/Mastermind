b9f1aa403909934562cf69bd31e87e4e66cd8e99
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2023-12-03 13:22:44 +0200

    Refactor complete

--- game.rb
+++ game.rb
@@ -1,52 +1,82 @@
-require_relative 'colors'
 require_relative 'player'
 require 'pry-byebug'
+
 # This is my Game class, rubycop stop pestering me
 class Game
-  @board = []
-  @latest_guess = ''
-  @feedback = ''
-  @code
-  @guess
+  attr_accessor :board, :round
+  def initialize
+    @board = []
+    @feedback = []
+    @code = []
+    @guess = []
+    @round = 0
+    @difficulty = 12
+    @pegs = []
+  end
 
   def user_input
     @input = gets.chomp
     # missing validation
   end
 
+  # def difficulty
+  #   puts "Select number of guesses: 16 for easy, 12 for normal, 8 for hard"
+  #   @difficulty = gets.chomp.to_i
+  # end
+
   def guess
     puts 'Enter your guess: '
     user_input
-    @latest_guess = Player.input(@input)    
-    @guess = @input.chars
+    @pegs[@round] = Player.input(@input)
+    @guess[@round] = @input.chars
+    @input = nil
   end
 
   def code
-    puts 'Enter the code:'
+    puts 'Enter the code: '
     user_input
+    system("clear")
+    system ("cls")
     @code = @input.chars
-  end
-
-  def add_guess_to_board
-    @board.push("#{@latest_guess}    #{@feedback}")
+    @input = nil
   end
 
   def feedback
-    @feedback = Player.feedback(@code, @guess)
-    add_guess_to_board
+    @feedback[@round] = Player.feedback(@code, @guess[@round])
   end
 
   def print_board
-    feedback
-    @board.each {|a| puts a}
+    (0..@round).each do |i|
+      puts "#{@pegs[i]} | #{@feedback[i]}"
+    end
   end
 
   def start 
-   # Player.set_difficulty  #not implemented yet
+    # difficulty
     code
-    binding.pry
+    until (win? || end?) do
     guess
+    feedback
     print_board
+    @round += 1
+    end
+    if win?
+       puts " Congrats, you broke the code!" 
+    else 
+      puts "On no, you lost"
+    end
+  end
+
+  def end?
+    if @round == @difficulty
+      true
+    end
+  end
+
+  def win?
+    if @guess[@round -1] == @code
+      true
+    end
   end
 end
 
--- player.rb
+++ player.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+require_relative 'colors'
 class Player
     @@difficulty
     @@peg1 = ' ' << "\u278a".red
@@ -6,11 +8,9 @@ class Player
     @@peg4 = ' ' << "\u278d".blue
     @@peg5 = ' ' << "\u278e".pink
     @@peg6 = ' ' << "\u278f".gray
-    @@potition_hit = ' ' << "\u25cf".red
-    @@color_hit = ' ' << "\u26ac"
-    @@latest_guess = ''
-    @@feedback = ''
-
+    @@position_hit = "\u25cf".red
+    @@color_hit = "\u26ac"
+    
     def self.set_difficulty
         puts "-----Select the difficulty------"
         puts "Type [1] for Easy(16 guesses), [2] for Normal(12 guesses),
@@ -32,40 +32,47 @@ class Player
         end
     end
     def self.input(input)
+        latest_guess = ""
         input.chars.each do |a|
           case a
           when '1'
-            @@latest_guess << @@peg1
+            latest_guess << @@peg1
           when '2'
-            @@latest_guess << @@peg2
+            latest_guess << @@peg2
           when '3'
-            @@latest_guess << @@peg3
+            latest_guess << @@peg3
           when '4'
-            @@latest_guess << @@peg4
+            latest_guess << @@peg4
           when '5'
-            @@latest_guess << @@peg5
+            latest_guess << @@peg5
           when '6'
-            @@latest_guess << @@peg6
+            latest_guess << @@peg6
           end
         end
-        @@latest_guess
+        latest_guess
       end
 
-      def self.feedback (code, guess)
-        code.each do |b|
-            for i in guess.length-1
-                if guess[i] == b
-                    @@feedback << @@potition_hit
-                    guess.delete_at[i]
-                end
-            end
+      # 
+      def self.feedback(code, guess)
+        feedback = ""
+        guess_copy = guess.dup
+      
+        code.each_with_index do |b, i|
+          if guess_copy[i] == b
+            feedback << @@position_hit
+            guess_copy[i] = nil
+          end
         end
-        
-        guess.uniq.each do |a| 
-            if code.incluce?(a)
-                @@feedback << @@color_hit
-           end
+      
+        guess_copy.uniq.each do |a|
+          if code.include?(a)
+            feedback << @@color_hit
+          end
         end
+        feedback
       end
-      @@feedback    
+
+      def self.display_choice
+        @@peg1<<@@peg2<<@@peg3<<@@peg4<<@@peg5<<@@peg6
+      end
 end
